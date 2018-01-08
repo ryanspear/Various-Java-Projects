f9dd7ef38fba363333f6b680b8a17a8d8a782ba7
rspear <rspear@oucs1433.otago.ac.nz>
2018-01-08 16:56:55 +1300

    Given Kral class a constructor which effect the parent (super), works fine with the parent class now, ust need to make a couple more methods for handleRoll, SwapDice, etc

--- Kral.java
+++ Kral.java
@@ -1,19 +1,36 @@
 package rollin;
 
 import java.util.*;
+  
 
-public class Kral{
+public class Kral extends Rollin{
+    static int dice[];
 
-
-    public static void main(String [] args){
-        int dice[] = new int[6];
-        dice = firstRoll();
-        System.out.println("Woohoo");
+    public Kral(int[] dice){
+        super(dice);
+    }
         
+
+    /**
+     * Main method initialises dice with random integers.
+     * Getted is just a lil trial array to make sure getDice works, which it
+     * looks like it does right now
+     */
+    
+    public static void main(String [] args){
+        dice = firstRoll();
+        Kral obj = new Kral(dice);
+       
+        int[] getted = obj.getDice();
+        for(int i = 0; i < 6; i++){
+          System.out.println(getted[i]); 
+          }
 
     }
 
-
+    /** initalises the first 6 dice as random integers from 1-6
+     * @return the array of 6 random die values
+     */
     public static int[] firstRoll(){
         int roll[] = new int[6];
         Random rnd = new Random();
@@ -26,6 +43,14 @@ public class Kral{
 
         return roll;
 
+    }
+    /** this needs to be here as it is an abstract method in the parent class
+     * (I think) We will write the method in this class possibly? Maybe
+     * it doesn't matter where we write it. Find this out.
+     */ 
+    public int handleRoll(int roll){
+
+        return 1;
     }
     
 
